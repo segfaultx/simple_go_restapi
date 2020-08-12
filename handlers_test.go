33fe096715434f8d21b09919e79aedf4e60ea717
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-08-12 19:43:11 +0200

    added initial handler test

---
+++ handlers_test.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	repo "gitlab.com/amatusm/simpleREST/pkg/repo"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type mockRepo struct {
+	Products []repo.Product
+}
+
+func (r *mockRepo) AddProduct(product repo.Product) error {
+	if product.Id == -1 {
+		return errors.New("-1 is the signal from test to throw an error")
+	}
+	r.Products = append(r.Products, product)
+	return nil
+}
+
+func (r *mockRepo) UpdateProduct(product repo.Product) error {
+	for _, item := range r.Products {
+		if item.Id == product.Id {
+			item.Name = product.Name
+			return nil
+		}
+	}
+	return errors.New("could not find requested item")
+}
+
+func (r *mockRepo) AllProducts() []repo.Product {
+	return r.Products
+}
+
+func (r *mockRepo) InitRepo() {
+	return
+}
+
+func (r *mockRepo) RemoveProduct(product repo.Product) error {
+	for index, item := range r.Products {
+		if (item.Id == product.Id) && (item.Name == product.Name) {
+			r.Products = append(r.Products[:index], r.Products[index+1:]...)
+			return nil
+		}
+	}
+
+	return errors.New("couldn't update product")
+}
+
+var repository mockRepo
+
+func initMockRepo() {
+	testProducts := make([]repo.Product, 0)
+	testProduct := repo.Product{
+		Id:   1,
+		Name: "Hosen",
+	}
+	testProducts = append(testProducts, testProduct)
+	repository = mockRepo{
+		Products: testProducts,
+	}
+}
+
+func TestMakeAllProductsHandlerGET(t *testing.T) {
+	initMockRepo()
+
+	req, err := http.NewRequest("GET", "/catalog/products", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := MakeAllProductsHandler(&repository)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("unexpected statuscode, got %d expected %d", status, http.StatusOK)
+	}
+	expected, _ := json.Marshal(repository.Products)
+	if response := rr.Body.String(); response != string(expected) {
+		t.Errorf("unexpected responsebody, got %s wanted %s", response, expected)
+	}
+}
+
+func TestMakeAllProductsHandlerPOST(t *testing.T) {
+	initMockRepo()
+
+	newProduct := repo.Product{Id: 2, Name: "Schuhe"}
+	newProductJson, _ := json.Marshal(newProduct)
+	newProducts := make([]repo.Product, 0)
+	newProducts = append(newProducts, repository.Products...)
+
+	reader := bytes.NewReader(newProductJson)
+	req, err := http.NewRequest("POST", "/catalog/products", reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := MakeAllProductsHandler(&repository)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("unexpected statuscode, got %d expected %d", status, http.StatusOK)
+	}
+
+	expected, _ := json.Marshal(newProduct)
+	if response := rr.Body.String(); response != string(expected) {
+		t.Errorf("unexpected responsebody, got %s wanted %s", response, expected)
+	}
+}
+
+func TestMakeAllProductsHandlerPOSTFail(t *testing.T) {
+	initMockRepo()
+	reader := bytes.NewReader([]byte("aiusazdvawldkab"))
+	req, err := http.NewRequest("POST", "/catalog/products", reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := MakeAllProductsHandler(&repository)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("unexpected statuscode, got %d expected %d", status, http.StatusInternalServerError)
+	}
+	failproduct, _ := json.Marshal(repo.Product{Id: -1, Name: "bloe"})
+	reader = bytes.NewReader(failproduct)
+	req, err = http.NewRequest("POST", "/catalog/products", reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("unexpected statuscode, got %d expected %d", status, http.StatusInternalServerError)
+	}
+}
+
+func TestMakeProductsHandler(t *testing.T) {
+	initMockRepo()
+
+}
