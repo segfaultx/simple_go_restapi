6f6103bfd84f44996e77363309249b58ff0a4f25
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-08-14 14:12:31 +0200

    added repo close to main

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"gitlab.com/amatusm/simpleREST/pkg/repo"
+	"log"
 	"net/http"
+	"os"
 )
-
 
 func main() {
 	r := mux.NewRouter()
 	repository := repo.DefaultRepository{Products: nil}
-	repository.InitRepo()
+	defer repository.Close()
+	user := os.Getenv("POSTGRES_USER")
+	passwd := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DBNAME")
+	err := repository.InitRepo(user, passwd, dbname)
+	if err != nil {
+		log.Fatal(err)
+	}
 	r.HandleFunc("/catalog/products/{id}", MakeProductsHandler(&repository)).Methods("GET", "DELETE", "PUT")
 	r.HandleFunc("/catalog/products", MakeAllProductsHandler(&repository)).Methods("GET", "POST")
 	_ = http.ListenAndServe(":8080", r)
