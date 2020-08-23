5d9e7b17e181011ad37dfb7bdffb9c3bb3bd6ece
Amatus Muscheites <amatus.muscheites@gmail.com>
2020-08-23 14:30:35 +0200

    implemented register method, moved role to repo

--- pkg/auth/authservice.go
+++ pkg/auth/authservice.go
@@ -1,9 +1,11 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/segfaultx/simple_rest/pkg/repo"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"os"
 	"time"
@@ -15,24 +17,17 @@ type (
 	AuthenticationService interface {
 		GenerateToken(credentials Credentials) (string, error)
 		GetTokenFromString(tokenString string) (*jwt.Token, error)
+		RegisterUser(username, password string) error
 	}
 
 	Credentials struct {
 		Password string `json:"password"`
 		Username string `json:"username"`
-		Role     Role `json:"roles"`
 	}
-
-	Role string
 
 	BasicJwtAuthService struct {
 		repo repo.UserRepository
 	}
-)
-
-const (
-	ADMIN Role = "ADMIN"
-	USER Role = "USER"
 )
 
 func New(repository repo.UserRepository) AuthenticationService {
@@ -41,14 +36,37 @@ func New(repository repo.UserRepository) AuthenticationService {
 	return authService
 }
 
+func (authService *BasicJwtAuthService) RegisterUser(username, password string) error {
+	_, err := authService.repo.GetByUsername(username)
+	if err == nil {
+		return errors.New("username already taken")
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	usr := repo.User{Username: username, Password: string(hashedPassword), Role: repo.USER}
+	return authService.repo.AddUser(usr)
+}
+
 func (authService *BasicJwtAuthService) GenerateToken(credentials Credentials) (string, error) {
+	usr, err := authService.repo.GetByUsername(credentials.Username)
+	if err != nil {
+		return "", err
+	}
+	err = checkPassword(usr, credentials)
+	if err != nil {
+		return "", err
+	}
 	claims := make(jwt.MapClaims)
 	claims["authorized"] = true
-	claims["userId"] = credentials.Username
+	claims["userId"] = usr.Username
+	claims["role"] = usr.Role
 	claims["exp"] = time.Now().Add(10 * time.Minute).Unix()
 	token := jwt.New(jwt.SigningMethodHS256)
 	token.Claims = claims
 	return token.SignedString(jwtKey)
+}
+
+func checkPassword(user repo.User, credentials Credentials) error {
+	return bcrypt.CompareHashAndPassword([]byte(credentials.Password), []byte(user.Password))
 }
 
 func (authService *BasicJwtAuthService) GetTokenFromString(tokenString string) (*jwt.Token, error) {
